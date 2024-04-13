ed633b30b6335a081e3b760ba430c71e9a33eb23
nivescho <danarodaschacon@gmail.com>
2024-04-13 11:41:23 -0500

    ya

---
+++ assets/js/proye.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var btnencrip = document.getElementById("btnencrip");
+    var btnsonmusic = document.getElementById("btnsonmusic");
+
+    
+    btnencrip.addEventListener("click", function() {
+       
+        window.location.href = "encriptador.html";
+    });
+
+ 
+    btnsonmusic.addEventListener("click", function() {
+    
+        window.location.href = "emprendimiento.html";
+    });
+});
--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var btnProyectos = document.getElementById("btnProyectos");
     var btnNosotras = document.getElementById("btnNosotras");
+    var btnshinge = document.getElementById("btnshinge");
 
     
     btnProyectos.addEventListener("click", function() {
@@ -14,4 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
     
         window.location.href = "nosotras.html";
     });
+
+    btnshinge.addEventListener("click", function() {
+       
+        window.location.href = "shingeki.html";
+    });
+
 });
